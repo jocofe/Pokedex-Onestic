780543027dbeef410e700b2585ca1a0b9c3075dd
José <helloyinx@gmail.com>
2024-07-10 10:22:42 +0200

    Creating Routes, Global Variable CSS and Button component.

---
+++ src/components/buttons/Button.tsx
@@ -0,0 +1,34 @@
+import classNames from 'classnames';
+import { ButtonProps } from '../../models/button';
+
+export const Button = ({
+	className = '',
+	component = 'button',
+	size = 'default',
+	disabled,
+	color = 'primary',
+	type = 'button',
+	children,
+	onClick,
+	...rest
+}: ButtonProps) => {
+	const btnClass = classNames({
+		btn: true,
+		[className]: className,
+		[`btn--${size}`]: size,
+		[`btn--${color}`]: color,
+		'btn--disabled': disabled,
+	});
+
+	const Component = component;
+	return (
+		<Component
+			className={btnClass}
+			type={type}
+			disabled={disabled}
+			onClick={onClick}
+			{...rest}>
+			{children}
+		</Component>
+	);
+};
---
+++ src/views/pokemon-page/PokemonPage.tsx
@@ -0,0 +1,3 @@
+export const PokemonPage = () => {
+	return <div>Pokemon Page</div>;
+};
