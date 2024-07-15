c79e52d69877a542b0c5ffdc9c3d9ce9be70896e
José <helloyinx@gmail.com>
2024-07-15 10:03:23 +0200

    Fixed SearchBar in favorites.

--- src/views/favorites/Favorites.tsx
+++ src/views/favorites/Favorites.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useMemo } from 'react';
 import { Button } from '../../components/buttons/Button';
 import { PokemonCard } from '../../components/pokemon-card/PokemonCard';
 import { Link, NavLink } from 'react-router-dom';
@@ -9,10 +9,12 @@ import { PaginationControls } from '../../components/pagination-control/Paginati
 import { PokemonListViewToggle } from '../../components/pokemon-list-view-toggle.tsx/PokemonListViewToggle';
 import { usePagination } from '../../hooks/usePagination';
 import { FavoritesContext } from '../../context/FavoriteProvider';
+import { useSearch } from '../../hooks/useSearch';
 
 export const Favorites = () => {
 	const [currentPage, setCurrentPage] = useState(1);
 	const [isListView, setIsListView] = useState(false);
+	const { searchTerm } = useSearch();
 	const { favorites } = useContext(FavoritesContext);
 	const { pokemonList, error, isLoading } = useFetchPokemonSinnoh();
 
@@ -20,8 +22,14 @@ export const Favorites = () => {
 		favorites.includes(String(pokemon.id))
 	);
 
+	const filteredPokemonList = useMemo(() => {
+		return favoritePokemon.filter((pokemon) =>
+			pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+		);
+	}, [favoritePokemon, searchTerm]);
+
 	const { paginatedItems: visiblePokemon, totalPages } = usePagination({
-		items: favoritePokemon,
+		items: filteredPokemonList,
 		currentPage,
 		itemsPerPage: 12,
 	});
